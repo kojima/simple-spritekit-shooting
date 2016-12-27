9af1e82d684a74824894e0f2b2e10ad502288041
Hidenori Kojima <hide.k0712@gmail.com>
2016-12-27 22:51:19 +0900

    Removed the unused codes. Added the player spaceship.

--- simple-shooting-game/GameViewController.swift
+++ simple-shooting-game/GameViewController.swift
@@ -17,14 +17,13 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = GameScene(size: view.frame.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFit
             
+            // Present the scene
+            view.presentScene(scene)
+
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
